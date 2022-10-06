e00499a9d7b84f201aa92e86c69e7dd45d57cd65
AlefReis20 <aleftec@outlook.com>
2022-10-06 00:03:30 -0300

    finalized

--- scripts.js
+++ scripts.js
@@ -1,24 +1,19 @@
 const inputs = document.querySelectorAll('input')
 const button = document.querySelector('button')
 
-const redirect = () => {
-    window.location.href = "./thankyou-page.html"
-}
-
 const checkInputs = (inputs) => {
     let filled = true
-    inputs.forEach(function(input) {
-
-        if(input.value === '')
-        filled = false  
+    inputs.forEach((input) => {
+        if (input.value === '')
+            filled = false
     });
 
     return filled
 }
 
-inputs.forEach(function(input) {
+inputs.forEach((input) => {
     const inputIsOk = () => {
-        if(checkInputs(inputs)) {
+        if (checkInputs(inputs)) {
             button.disabled = false
         } else {
             button.disabled = true
@@ -27,47 +22,26 @@ inputs.forEach(function(input) {
     input.addEventListener('click', inputIsOk)
 })
 
-const input1 = document.getElementById('input1')
-const input2 = document.getElementById('input2')
-const input3 = document.getElementById('input3')
-const input4 = document.getElementById('input4')
-const input5 = document.getElementById('input5')
+const selected = (n) => {
+    let x = inputs
+    for (i = 0; i < x.length; i++) {
+        x[i].style.background = "#2F474F"
+    }
+    document.getElementById(n).style.background = "#FB7413"
+    document.getElementById(n).style.color = "#FFF"
 
-const clickInput1 = () => {
-        input1.style.background = '#FB7312'
-        input1.style.color = '#FFF'  
+    document.getElementById('paragraph').textContent = `You selected ${n} out of 5!`
 }
 
-const clickInput2 = () => {
-        input2.style.background = '#FB7312'
-        input2.style.color = '#FFF'
+const pressedButton = () => {
+    const container = document.getElementById('box1')
+    const container2 = document.getElementById('box2')
+    if (container.style.display === "none") {
+        container.style.display = "block"
+        container2.style.display = "none"
+    } else {
+        container.style.display = "none"
+        container2.style.display = "block"
+    }
 }
-
-const clickInput3 = () => {
-    input3.style.background = '#FB7312'
-    input3.style.color = '#FFF'
-}
-
-const clickInput4 = () => {
-    input4.style.background = '#FB7312'
-    input4.style.color = '#FFF'
-}
-
-const clickInput5 = () => {
-    input5.style.background = '#FB7312'
-    input5.style.color = '#FFF'
-}
-
-input1.addEventListener('click', clickInput1)
-input2.addEventListener('click', clickInput2)
-input3.addEventListener('click', clickInput3)
-input4.addEventListener('click', clickInput4)
-input5.addEventListener('click', clickInput5)
-
-const paragraph = document.getElementById('paragraph')
-const newParagraph = () => {
-if(clickInput1==='1') {
-    paragraph.innerText = 'seu texto aqui'
-}
-}
-paragraph.addEventListener('load',newParagraph)
+button.addEventListener('click', pressedButton)
